1a9fa0963e5fa27e1bb25f2a2a22c62be659ac48
TJ <michael.btj@hotmail.com>
2021-11-24 07:57:56 +0000

    added countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+// HELPER FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const countOnly = function(allItems, itemsToCount) {
+  let ret = {}
+  for (element of allItems) {
+    if (itemsToCount[element]) {
+      console.log(ret);
+      ret[element] === undefined ? ret[element] = 1 : ret[element]++;
+    }
+  }
+  console.log(ret);
+  return ret;
+
+};
+
+// TEST CASES
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
