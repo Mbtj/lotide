7a12d471a1b4cc24ce2a4cfc9741bc1595225661
TJ <michael.btj@hotmail.com>
2021-11-24 15:43:53 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// TEST FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// MAIN FUNCTION
+const findKeyByValue = function(obj, value) {
+  for (const [key, val] of Object.entries(obj)) {
+    if (val === value) {
+      return key;
+    }
+  }
+};
+
+// TEST CASES
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
