367bf835f25c5bed2470bd2dac99fa6e3a0f1049
TJ <michael.btj@hotmail.com>
2021-11-24 17:00:37 +0000

    update eqObjects with non-primitive cases

--- eqObjects.js
+++ eqObjects.js
@@ -7,6 +7,20 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// HELPER FUNCTIONS
+const eqArrays = function(actualArr, expectedArr) {
+  if (actualArr.length != expectedArr.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actualArr.length; i++) {
+      if (actualArr[i] !== expectedArr[i]) {
+        return false;
+      }
+    }
+  }
+  return true; // arrays have passed all cases.
+}
+
 
 // ACTUAL FUNCTION
 const eqObjects = function(actual, expected) {
@@ -15,7 +29,10 @@ const eqObjects = function(actual, expected) {
 
   for (const key in actual) {
     actualSize++;
-    if (actual[key] !== expected[key]) {
+    if (actual[key] !== expected[key] // check primitive
+      && (Array.isArray(actual[key]) && Array.isArray(expected[key]) && !eqArrays(actual[key], expected[key])) // check array
+      && (typeof(actual) && typeof(expected) && !eqObjects(actual[key], expected[key]))) { // check object using recursive call
+
       return false;
     }
   }
@@ -27,10 +44,22 @@ const eqObjects = function(actual, expected) {
   return actualSize === expectedSize;
 };
 
-// TEST
+// TEST PRIMITIVE
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
+
+// TEST ARRAY
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
+// TEST OBJECT
+assertEqual(eqObjects({ab, abc}, {abc, ab}), true); // => true
+assertEqual(eqObjects({abc, cd}, {abc, abc}), false); // => false
