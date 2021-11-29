900d0a40ec18a7c3fb6379c5cedbeb601766384c
TJ <michael.btj@hotmail.com>
2021-11-29 16:53:29 +0000

    tail and middle test files now use M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,22 @@
 
+const { assert } = require("chai");
 const middle = require("../middle");
 
-// TEST CASES
-assertArraysEqual(middle([0]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+// // TEST CASES
+// assertArraysEqual(middle([0]), []);
+// assertArraysEqual(middle([1,2,3]), [2]);
+// assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+
+describe('#middle', () => {
+  it('returns empty array when given a singleton', () => {
+    assert.deepEqual(middle([0]), []);
+  });
+
+  it('returns [2] when given [1, 2, 3]', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it('returns [3, 4] when given [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,23 @@
+const { assert } = require('chai');
 const tail = require('../tail.js');
-const assertEqual = require('../assertEqual');
+
 
 
 // TEST CASES
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-console.log(tail([1,2,3]));
-console.log(tail([]));
+// console.log(tail([1,2,3]));
+// console.log(tail([]));
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] when  given ["Hello", "Lighthouse", "Labs"]',() => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+
+});
