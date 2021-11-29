839f3887260f59823df6ac76398f164fdb9cad4a
TJ <michael.btj@hotmail.com>
2021-11-29 16:12:42 +0000

    refactor and test eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-// HELPER FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+assertEqual = require('./assertEqual');
 
 const eqArrays = function(actual, expected) {
   if (actual.length != expected.length) {
@@ -20,10 +13,4 @@ const eqArrays = function(actual, expected) {
   return true; // arrays have passed all cases.
 }
 
-// TEST CASES
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])), true; // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+module.exports = eqArrays;
