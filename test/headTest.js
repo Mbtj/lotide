4ad268930d03f5d01e90d34733628518eb44c39c
TJ <michael.btj@hotmail.com>
2021-11-29 16:30:46 +0000

    headTest now uses M&C testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,21 @@
-head = require('../head.js')
+const head = require('../head.js');
+const assert = require('chai').assert;
 
-// Test cases
+// // Test cases
 
 
-console.log(head([]));
-console.log(head([3]));
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// console.log(head([]));
+// console.log(head([3]));
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe('#head', () => {
+  it('should return 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+
